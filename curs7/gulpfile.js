6cd68ff087937b5845add583f237ca5a6d6505a1
tataru dragos <tataru.dragos@yahoo.com>
2022-03-22 20:47:29 +0200

    curs7

---
+++ curs7/gulpfile.js
@@ -0,0 +1,9 @@
+"use strict"
+const gulp =require ("gulp");
+const sass =require("gulp-sass") (require("node-sass"));
+
+gulp.task("sass" ,function () {
+    return gulp.src("sass/ap6.scss")
+    .pipe(sass().on("error" ,sass.logError))
+    .pipe(gulp.dest('css'));
+});
